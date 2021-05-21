c28c56b377c6bbbb076aae6809f4a3a91d4b1eec
TriHalder2 <73516625+TriHalder2@users.noreply.github.com>
2021-05-21 09:31:18 +0530

    Add files via upload

---
+++ junit/NotEnoughFundsException.java
@@ -0,0 +1,9 @@
+package JUnit_Activities;
+
+public class NotEnoughFundsException extends Exception {
+
+	public NotEnoughFundsException(Integer amount, Integer balance) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
